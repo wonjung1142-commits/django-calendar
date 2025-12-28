286bccf2b519a1516f0e967dd63b8a2d8629e9e2
wonjung1142 <wonjung1142@gmail.com>
2025-12-28 16:37:32 +0900

    prepare railway deploy

--- config/settings.py
+++ config/settings.py
@@ -116,4 +116,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+
+ALLOWED_HOSTS = ["*"]  # 배포 테스트 단계에서는 OK

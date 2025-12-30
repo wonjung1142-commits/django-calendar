738ceb3f1a47b7c75eee1c9bde997c89907d7a1c
wonjung1142 <wonjung1142@gmail.com>
2025-12-30 16:37:49 +0900

    제발되라

--- calendarapp/forms.py
+++ calendarapp/forms.py
@@ -5,21 +5,21 @@ from .models import Event
 class EventForm(forms.ModelForm):
     class Meta:
         model = Event
-        # 모델에 실제 존재하는 필드명인지 다시 한번 확인해주세요!
-        # (만약 'employee'가 아니라 'user'라면 'user'로 바꿔야 합니다)
+        # 모델 필드명과 정확히 일치합니다
         fields = ['employee', 'leave_type', 'start', 'end']
 
         widgets = {
             'employee': forms.Select(attrs={'class': 'form-control'}),
             'leave_type': forms.Select(attrs={'class': 'form-control'}),
-            'start': forms.DateTimeInput(attrs={
+            # DateInput으로 변경하여 모델의 DateField와 형식을 맞춥니다.
+            'start': forms.DateInput(attrs={
                 'class': 'form-control',
-                'type': 'datetime-local',
-            }, format='%Y-%m-%dT%H:%M'),
-            'end': forms.DateTimeInput(attrs={
+                'type': 'date',  # 시간 없이 날짜만 선택하는 창으로 변경
+            }),
+            'end': forms.DateInput(attrs={
                 'class': 'form-control',
-                'type': 'datetime-local',
-            }, format='%Y-%m-%dT%H:%M'),
+                'type': 'date',
+            }),
         }
 
     def __init__(self, *args, **kwargs):
